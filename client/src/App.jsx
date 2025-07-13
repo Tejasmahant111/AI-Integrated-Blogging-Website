dd4123ac0a5a0c47d9993cc1a725fe5d1415cdef
satyamt1204 <satyamtek1204@gmail.com>
2025-07-13 17:31:29 +0530

    admin page and frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import 'quill/dist/quill.snow.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Login from './components/admin/Login'
 import Blog from './pages/Blog'
-
+import Home from './pages/Home'
+import AddBlog from './pages/admin/AddBlog'
+import Comments from './pages/admin/Comments'
+import Dashboard from './pages/admin/Dashboard'
+import Layout from './pages/admin/Layout'
+import ListBlog from './pages/admin/ListBlog'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/admin' element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   )
